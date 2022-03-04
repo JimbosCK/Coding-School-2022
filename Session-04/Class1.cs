a3ffd4fa178c5f8e487ff1d76ed0dd2e946a1bc6
Jimbos <tr9l9l9@hotmail.com>
2022-03-04 13:23:05 +0200

    Added Class1 that implements the reversal of a string, and answers the first question of the excersise.

---
+++ Session-04/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_04 {
+    internal class Class1 {
+
+
+        public Class1(){
+            
+        }  
+
+        public string ReverseString(string str){
+            if (str == null) { return string.Empty; }
+            
+            string reversedString = string.Empty;
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reversedString += str[i];
+            }
+
+            return reversedString;
+        }
+    }
+}
--- Session-04/Program.cs
+++ Session-04/Program.cs
@@ -1,2 +1,14 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using Session_04;
+
+//--------- 1 ---------
+Console.WriteLine("Please enter a string: ");
+string inputString = Console.ReadLine();
+
+var reverser = new Class1();
+
+reverser.ReverseString(inputString);
+
+Console.WriteLine($"Reversed String: {reverser.ReverseString(inputString)}");
+
+
+
