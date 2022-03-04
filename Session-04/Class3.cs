d439b6c09de8c09c859075bc125e8cb923bd79e7
Jimbos <tr9l9l9@hotmail.com>
2022-03-04 14:11:03 +0200

    Added Class3 that calculates all prime numbers in range (1..n)

---
+++ Session-04/Class3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_04 {
+    internal class Class3 {
+
+        public Class3(){
+
+        }
+
+        public string FindPrimeNumbers(int limit){
+            string primeNumbers = string.Empty;
+            bool isPrime = true;
+
+            //2 is the only even prime number so this is the only even number we will check/add.
+            if (limit >= 2) {
+                primeNumbers += "2";
+            }
+            for (int i = 3; i <= limit; i+=2){
+                for (int j = i-2; j >= 3; j-=2)
+                {
+                    if (i % j == 0){
+                        isPrime = false;
+                        break; //not prime
+                    }
+                }
+                if (isPrime)
+                {
+                    primeNumbers += ", " + Convert.ToString(i);
+                }
+                else {
+                    isPrime = true;
+                }
+            }
+
+            return primeNumbers;
+        }
+    }
+}
--- Session-04/Program.cs
+++ Session-04/Program.cs
@@ -12,12 +12,23 @@ Console.WriteLine($"Reversed String: {reverser.ReverseString(inputString)}");
 
 
 //--------- 2 ---------
-Console.WriteLine("Please enter a string: ");
+Console.WriteLine("Please enter a positive integer: ");
 inputString = Console.ReadLine();
 
 int inputInt = Convert.ToInt32(inputString);
 
 var calculator = new Class2();
 
-Console.WriteLine($"Calculated sum: {calculator.CalculateSum(inputInt)}\n" +
-    $"Calculated product: {calculator.CalculateProduct(inputInt)}");
+Console.WriteLine($"Calculated sum(1...{inputInt}): {calculator.CalculateSum(inputInt)}\n" +
+    $"Calculated product(1...{inputInt}): {calculator.CalculateProduct(inputInt)}");
+
+//--------- 3 ---------
+Console.WriteLine("Please enter a positive integer: ");
+inputString = Console.ReadLine();
+
+inputInt = Convert.ToInt32(inputString);
+
+var primeCalculator = new Class3();
+
+Console.WriteLine($"Prime numbers in range (1, {inputInt}): {primeCalculator.FindPrimeNumbers(inputInt)}");
+
