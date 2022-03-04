ece8999a5c059efddec7797c589ec63cd538b611
Jimbos <tr9l9l9@hotmail.com>
2022-03-04 14:41:27 +0200

    Added Class4 that performs the multiplication of all the elements between 2 1x4 Arrays and returns a 1x16 Array

---
+++ Session-04/Class4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_04 {
+    internal class Class4 {
+        public Class4()
+        {
+
+        }
+
+
+        public int[] Multiply2_4(int[] array1, int[] array2){
+            int[] resultArray = new int[16];
+
+            for (int i = 0; i < 4; i++){
+                for (int j = 0; j < 4; j++)
+                {
+                    resultArray[(i * 4) + j] = array1[i] * array2[j];
+                    //Console.WriteLine($"i*j= {i * j} | array1[i] * array2[j] = {array1[i] * array2[j]}");
+                }
+            }
+
+
+            return resultArray;    
+        }
+    }
+}
--- Session-04/Program.cs
+++ Session-04/Program.cs
@@ -1,18 +1,18 @@
 ﻿using Session_04;
 
 //--------- 1 ---------
-Console.WriteLine("Please enter a string: ");
+Console.WriteLine("-1-\nPlease enter a string: ");
 string inputString = Console.ReadLine();
 
 var reverser = new Class1();
 
 reverser.ReverseString(inputString);
 
-Console.WriteLine($"Reversed String: {reverser.ReverseString(inputString)}");
+Console.WriteLine($"Reversed String: {reverser.ReverseString(inputString)}\n");
 
 
 //--------- 2 ---------
-Console.WriteLine("Please enter a positive integer: ");
+Console.WriteLine("-2-\nPlease enter a positive integer: ");
 inputString = Console.ReadLine();
 
 int inputInt = Convert.ToInt32(inputString);
@@ -20,15 +20,27 @@ int inputInt = Convert.ToInt32(inputString);
 var calculator = new Class2();
 
 Console.WriteLine($"Calculated sum(1...{inputInt}): {calculator.CalculateSum(inputInt)}\n" +
-    $"Calculated product(1...{inputInt}): {calculator.CalculateProduct(inputInt)}");
+    $"Calculated product(1...{inputInt}): {calculator.CalculateProduct(inputInt)}\n");
 
 //--------- 3 ---------
-Console.WriteLine("Please enter a positive integer: ");
+Console.WriteLine("-3-\nPlease enter a positive integer: ");
 inputString = Console.ReadLine();
 
 inputInt = Convert.ToInt32(inputString);
 
 var primeCalculator = new Class3();
 
-Console.WriteLine($"Prime numbers in range (1, {inputInt}): {primeCalculator.FindPrimeNumbers(inputInt)}");
+Console.WriteLine($"Prime numbers in range (1, {inputInt}): {primeCalculator.FindPrimeNumbers(inputInt)}\n\n-4-");
 
+
+//--------- 4 ---------
+int[] Array1 = new int[4] { 2, 4, 9, 12 };
+int[] Array2 = new int[4] { 1, 3, 7, 10 };
+int[] resultArray = new int[16];
+var arrayMultiplicator = new Class4();
+
+resultArray = arrayMultiplicator.Multiply2_4(Array1, Array2);
+
+for (int i = 0; i < 16; i++){
+    Console.Write(resultArray[i] + ", ");
+}
