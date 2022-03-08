42e9a72b08ad49507f90ed64516d91742e07a097
Jimbos <tr9l9l9@hotmail.com>
2022-03-08 13:25:04 +0200

    Added base message class

---
+++ Session-05/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_05 {
+    internal class Message {
+
+        public Guid ID { get; }
+        public DateTime Timestamp { get; set; }
+        public string Text { get; set; }
+        public Message() { ID = Guid.NewGuid(); }
+    }
+}
