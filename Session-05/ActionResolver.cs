24875033579c2b535eaa23bc586fde205b75aa9d
Jimbos <tr9l9l9@hotmail.com>
2022-03-08 15:10:55 +0200

    Made first pass of the Uppercase funcionality

---
+++ Session-05/ActionResolver.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_05 {
+    internal class ActionResolver {
+
+        public MessageLogger Logger{ get; set; }
+
+        public ActionResolver() {
+            Logger = new MessageLogger();
+        }
+
+
+        public ActionResponse Excecute(ActionRequest request){
+            var response = new ActionResponse();
+            response.RequestID = request.RequestID;
+
+            switch (request.Action) {
+                case ActionEnum.Convert:
+                    break;
+                case ActionEnum.Uppercase:
+                    response.Output = MakeBiggestWordUpper(request.Input);
+                    Logger.Write( new Message() { 
+                        Text = $"Request [{request.RequestID}] : Attempting Uppercase on string: '{request.Input}'. Response output: '{response.Output}' .",
+                        Timestamp = DateTime.Now
+                    }); 
+                    break;
+                case ActionEnum.Reverse:
+                    break;
+                
+            }
+
+            return response;
+        }
+
+        //TO DO: [Refactor] MakeBiggestWordUpper
+        private string MakeBiggestWordUpper(string str){
+            int numOfWords = 0;
+            int maxLengthOfWord = 0;
+            int? indexOfBiggestWord = null;
+            string outputUpper = str;
+
+            string[] words = str.Split(' ');
+                for (int i = 0; i < words.Length; i++){
+                    if (words[i] != String.Empty){
+                        numOfWords++;
+                        if (words[i].Length > maxLengthOfWord){
+                            maxLengthOfWord = words[i].Length;
+                            indexOfBiggestWord = i;
+                        }
+                    }
+                }
+            if (indexOfBiggestWord != null && numOfWords > 1) {
+                words[(int)indexOfBiggestWord] = words[(int)indexOfBiggestWord].ToUpper();
+                outputUpper = string.Join(' ', words);
+            }
+            return outputUpper;
+        }
+
+    }
+}
--- Session-05/Program.cs
+++ Session-05/Program.cs
@@ -2,19 +2,27 @@
 
 //ActionEnum aE = ActionEnum.Convert;
 
-var mes1 = new Message
-{
-    Text = "This is the first message!"
-};
+//var mes1 = new Message
+//{
+//    Text = "This is the first message!"
+//};
 
-var mes2 = new Message
-{
-    Text = "This is the second message!!"
-};
+//var mes2 = new Message
+//{
+//    Text = "This is the second message!!"
+//};
 
-var mesLogger = new MessageLogger();
+//var mesLogger = new MessageLogger();
 
-mesLogger.Write(mes1);
-mesLogger.Write(mes2);
+//mesLogger.Write(mes1);
+//mesLogger.Write(mes2);
 
-Console.WriteLine(mesLogger.ReadAll());
+//Console.WriteLine(mesLogger.ReadAll());
+
+
+var resolver = new ActionResolver();
+var response = new ActionResponse();
+var request = new ActionRequest(){
+    Input ="This is a great day to go outside my friend! ",
+    Action =ActionEnum.Uppercase
+};response = resolver.Excecute(request);Console.Write(resolver.Logger.ReadAll());
