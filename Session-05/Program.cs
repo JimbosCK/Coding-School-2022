36a8a9c25affc37a4d0d81d0e5bfbb23398ad293
Jimbos <tr9l9l9@hotmail.com>
2022-03-09 08:46:01 +0200

    cleaned up comments in Programm.cs

--- Session-05/Program.cs
+++ Session-05/Program.cs
@@ -1,25 +1,5 @@
 ﻿using Session_05;
 
-//ActionEnum aE = ActionEnum.Convert;
-
-//var mes1 = new Message
-//{
-//    Text = "This is the first message!"
-//};
-
-//var mes2 = new Message
-//{
-//    Text = "This is the second message!!"
-//};
-
-//var mesLogger = new MessageLogger();
-
-//mesLogger.Write(mes1);
-//mesLogger.Write(mes2);
-
-//Console.WriteLine(mesLogger.ReadAll());
-
-
 var resolver = new ActionResolver();
 var response = new ActionResponse();
 var request1 = new ActionRequest() {
