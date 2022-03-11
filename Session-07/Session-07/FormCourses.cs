4dd8046c0ccfed0e3ae36c01c947169cd40e1297
Jimbos <tr9l9l9@hotmail.com>
2022-03-11 16:30:53 +0200

    Did Courses

--- Session-07/Session-07/FormCourses.cs
+++ Session-07/Session-07/FormCourses.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 
 namespace Session_07 {
     public partial class FormCourses : XtraForm {
+        private Uni.Course _selectedCourse;
         public List<Uni.Course> Courses { get; set; }
         public FormCourses() {
             InitializeComponent();
@@ -18,8 +19,93 @@ namespace Session_07 {
 
         private void FormCourses_Load(object sender, EventArgs e) {
             this.Text = "Courses";
-            TextEditCourseCode.Text = Courses[0].Code;
-            TextEditCourseSubject.Text = Courses[0].Subject;
+            FillList();
+
+
+        }
+        private void listBoxControl_SelectedIndexChanged(object sender, EventArgs e) {
+            if (ListBoxCourses.SelectedIndex > -1) {
+                SelectCourse();
+                DisplayCourse();
+            }
+
+        }
+        private void SelectCourse() {
+            int x = ListBoxCourses.SelectedIndex;
+            _selectedCourse = Courses[x];
+        }
+        private void DisplayCourse() {
+
+            if (_selectedCourse != null) {
+                TextEditCourseSubject.Text = _selectedCourse.Subject;
+                TextEditCourseCode.Text = _selectedCourse.Code;
+            }
+            else {
+                TextEditCourseSubject.Text = String.Empty;
+                TextEditCourseCode.Text = String.Empty;
+            }
+        }
+
+        private void UpdateCourse() {
+
+            if (_selectedCourse != null) {
+                _selectedCourse.Subject = TextEditCourseSubject.Text;
+                _selectedCourse.Code = TextEditCourseCode.Text;
+            }
+        }
+
+        private void ButtonDelete_Click(object sender, EventArgs e) {
+            DeleteCourse();
+        }
+        private void FillList() {
+            ListBoxCourses.Items.Clear();
+            if (Courses != null) {
+                foreach (var cr in Courses) {
+                    if (cr != null)
+                        ListBoxCourses.Items.Add(string.Format("{0} - {1} ", cr.Subject, cr.Code));
+                }
+            }
+            else {
+                Courses.Add(CreateNewCourse());
+            }
+
+        }
+        private void DeleteCourse() {
+
+            if (_selectedCourse != null) {
+
+                Courses.Remove(_selectedCourse);
+
+                _selectedCourse = null;
+
+                FillList();
+            }
+            DisplayCourse();
+        }
+
+        private void ButtonNew_Click(object sender, EventArgs e) {
+            Uni.Course newCourse = CreateNewCourse();
+            Courses.Add(newCourse);
+            ListBoxCourses.SelectedIndex = Courses.IndexOf(newCourse);
+
+            FillList();
+
+        }
+        private Uni.Course CreateNewCourse() {
+            Uni.Course prof = new Uni.Course() {
+                Subject = "New..."
+            };
+            return prof;
+        }
+
+        private void ButtonSave_Click(object sender, EventArgs e) {
+            UpdateCourse();
+            FillList();
+            MessageBox.Show("Changes have been saved.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void ButtonCancel_Click(object sender, EventArgs e) {
+            this.Close();
         }
     }
 }
