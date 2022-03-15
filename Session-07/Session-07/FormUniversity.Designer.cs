6f79fee3ffec4857c7ea0de4b37aa524cb033d34
Jimbos <tr9l9l9@hotmail.com>
2022-03-15 20:22:34 +0200

    changed University form to work with data bindings

--- Session-07/Session-07/FormUniversity.Designer.cs
+++ Session-07/Session-07/FormUniversity.Designer.cs
@@ -23,13 +23,16 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
             this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
-            this.TextEditUniversityYearsInService = new DevExpress.XtraEditors.TextEdit();
+            this.SpinEditYearsInService = new DevExpress.XtraEditors.SpinEdit();
             this.TextEditUniversityName = new DevExpress.XtraEditors.TextEdit();
             this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
             this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
             this.emptySpaceItem1 = new DevExpress.XtraLayout.EmptySpaceItem();
-            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.emptySpaceItem5 = new DevExpress.XtraLayout.EmptySpaceItem();
+            this.layoutControlItem5 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.emptySpaceItem4 = new DevExpress.XtraLayout.EmptySpaceItem();
             this.layoutControl2 = new DevExpress.XtraLayout.LayoutControl();
             this.ButtonCancel = new DevExpress.XtraEditors.SimpleButton();
             this.ButtonSave = new DevExpress.XtraEditors.SimpleButton();
@@ -37,25 +40,31 @@
             this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
             this.emptySpaceItem2 = new DevExpress.XtraLayout.EmptySpaceItem();
             this.layoutControlItem4 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.emptySpaceItem3 = new DevExpress.XtraLayout.EmptySpaceItem();
+            this.BSUniversity = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
             this.layoutControl1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.TextEditUniversityYearsInService.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SpinEditYearsInService.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.TextEditUniversityName.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl2)).BeginInit();
             this.layoutControl2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BSUniversity)).BeginInit();
             this.SuspendLayout();
             // 
             // layoutControl1
             // 
-            this.layoutControl1.Controls.Add(this.TextEditUniversityYearsInService);
+            this.layoutControl1.Controls.Add(this.SpinEditYearsInService);
             this.layoutControl1.Controls.Add(this.TextEditUniversityName);
             this.layoutControl1.Location = new System.Drawing.Point(12, 12);
             this.layoutControl1.Name = "layoutControl1";
@@ -64,21 +73,31 @@
             this.layoutControl1.TabIndex = 0;
             this.layoutControl1.Text = "layoutControl1";
             // 
-            // TextEditUniversityYearsInService
+            // SpinEditYearsInService
             // 
-            this.TextEditUniversityYearsInService.Location = new System.Drawing.Point(491, 12);
-            this.TextEditUniversityYearsInService.Name = "TextEditUniversityYearsInService";
-            this.TextEditUniversityYearsInService.Size = new System.Drawing.Size(168, 20);
-            this.TextEditUniversityYearsInService.StyleController = this.layoutControl1;
-            this.TextEditUniversityYearsInService.TabIndex = 5;
+            this.SpinEditYearsInService.EditValue = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.SpinEditYearsInService.Location = new System.Drawing.Point(99, 36);
+            this.SpinEditYearsInService.Name = "SpinEditYearsInService";
+            this.SpinEditYearsInService.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.SpinEditYearsInService.Properties.IsFloatValue = false;
+            this.SpinEditYearsInService.Properties.MaskSettings.Set("mask", "N00");
+            this.SpinEditYearsInService.Properties.NullText = "0";
+            this.SpinEditYearsInService.Size = new System.Drawing.Size(68, 20);
+            this.SpinEditYearsInService.StyleController = this.layoutControl1;
+            this.SpinEditYearsInService.TabIndex = 2;
             // 
             // TextEditUniversityName
             // 
-            this.TextEditUniversityName.Location = new System.Drawing.Point(106, 12);
+            this.TextEditUniversityName.Location = new System.Drawing.Point(99, 12);
             this.TextEditUniversityName.Name = "TextEditUniversityName";
-            this.TextEditUniversityName.Size = new System.Drawing.Size(287, 20);
+            this.TextEditUniversityName.Size = new System.Drawing.Size(240, 20);
             this.TextEditUniversityName.StyleController = this.layoutControl1;
-            this.TextEditUniversityName.TabIndex = 4;
+            this.TextEditUniversityName.TabIndex = 0;
             // 
             // Root
             // 
@@ -87,7 +106,9 @@
             this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
             this.layoutControlItem1,
             this.emptySpaceItem1,
-            this.layoutControlItem2});
+            this.emptySpaceItem5,
+            this.layoutControlItem5,
+            this.emptySpaceItem4});
             this.Root.Name = "Root";
             this.Root.Size = new System.Drawing.Size(671, 120);
             this.Root.TextVisible = false;
@@ -97,35 +118,51 @@
             this.layoutControlItem1.Control = this.TextEditUniversityName;
             this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
             this.layoutControlItem1.Name = "layoutControlItem1";
-            this.layoutControlItem1.Size = new System.Drawing.Size(385, 24);
+            this.layoutControlItem1.Size = new System.Drawing.Size(331, 24);
             this.layoutControlItem1.Text = "Name";
-            this.layoutControlItem1.TextSize = new System.Drawing.Size(82, 13);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(75, 13);
             // 
             // emptySpaceItem1
             // 
             this.emptySpaceItem1.AllowHotTrack = false;
-            this.emptySpaceItem1.Location = new System.Drawing.Point(0, 24);
+            this.emptySpaceItem1.Location = new System.Drawing.Point(0, 48);
             this.emptySpaceItem1.Name = "emptySpaceItem1";
-            this.emptySpaceItem1.Size = new System.Drawing.Size(651, 76);
+            this.emptySpaceItem1.Size = new System.Drawing.Size(651, 52);
             this.emptySpaceItem1.TextSize = new System.Drawing.Size(0, 0);
             // 
-            // layoutControlItem2
+            // emptySpaceItem5
             // 
-            this.layoutControlItem2.Control = this.TextEditUniversityYearsInService;
-            this.layoutControlItem2.Location = new System.Drawing.Point(385, 0);
-            this.layoutControlItem2.Name = "layoutControlItem2";
-            this.layoutControlItem2.Size = new System.Drawing.Size(266, 24);
-            this.layoutControlItem2.Text = "YearsI In Service";
-            this.layoutControlItem2.TextSize = new System.Drawing.Size(82, 13);
+            this.emptySpaceItem5.AllowHotTrack = false;
+            this.emptySpaceItem5.Location = new System.Drawing.Point(331, 0);
+            this.emptySpaceItem5.Name = "emptySpaceItem5";
+            this.emptySpaceItem5.Size = new System.Drawing.Size(320, 24);
+            this.emptySpaceItem5.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // layoutControlItem5
+            // 
+            this.layoutControlItem5.Control = this.SpinEditYearsInService;
+            this.layoutControlItem5.Location = new System.Drawing.Point(0, 24);
+            this.layoutControlItem5.Name = "layoutControlItem5";
+            this.layoutControlItem5.Size = new System.Drawing.Size(159, 24);
+            this.layoutControlItem5.Text = "Years in service";
+            this.layoutControlItem5.TextSize = new System.Drawing.Size(75, 13);
+            // 
+            // emptySpaceItem4
+            // 
+            this.emptySpaceItem4.AllowHotTrack = false;
+            this.emptySpaceItem4.Location = new System.Drawing.Point(159, 24);
+            this.emptySpaceItem4.Name = "emptySpaceItem4";
+            this.emptySpaceItem4.Size = new System.Drawing.Size(492, 24);
+            this.emptySpaceItem4.TextSize = new System.Drawing.Size(0, 0);
             // 
             // layoutControl2
             // 
             this.layoutControl2.Controls.Add(this.ButtonCancel);
             this.layoutControl2.Controls.Add(this.ButtonSave);
-            this.layoutControl2.Location = new System.Drawing.Point(12, 320);
+            this.layoutControl2.Location = new System.Drawing.Point(12, 321);
             this.layoutControl2.Name = "layoutControl2";
             this.layoutControl2.Root = this.layoutControlGroup1;
-            this.layoutControl2.Size = new System.Drawing.Size(660, 58);
+            this.layoutControl2.Size = new System.Drawing.Size(660, 57);
             this.layoutControl2.TabIndex = 1;
             this.layoutControl2.Text = "layoutControl2";
             // 
@@ -135,7 +172,7 @@
             this.ButtonCancel.Name = "ButtonCancel";
             this.ButtonCancel.Size = new System.Drawing.Size(317, 22);
             this.ButtonCancel.StyleController = this.layoutControl2;
-            this.ButtonCancel.TabIndex = 5;
+            this.ButtonCancel.TabIndex = 2;
             this.ButtonCancel.Text = "Cancel";
             this.ButtonCancel.Click += new System.EventHandler(this.ButtonCancel_Click);
             // 
@@ -145,7 +182,7 @@
             this.ButtonSave.Name = "ButtonSave";
             this.ButtonSave.Size = new System.Drawing.Size(315, 22);
             this.ButtonSave.StyleController = this.layoutControl2;
-            this.ButtonSave.TabIndex = 4;
+            this.ButtonSave.TabIndex = 0;
             this.ButtonSave.Text = "Save";
             this.ButtonSave.Click += new System.EventHandler(this.ButtonSave_Click);
             // 
@@ -158,7 +195,7 @@
             this.emptySpaceItem2,
             this.layoutControlItem4});
             this.layoutControlGroup1.Name = "layoutControlGroup1";
-            this.layoutControlGroup1.Size = new System.Drawing.Size(660, 58);
+            this.layoutControlGroup1.Size = new System.Drawing.Size(660, 57);
             this.layoutControlGroup1.TextVisible = false;
             // 
             // layoutControlItem3
@@ -175,7 +212,7 @@
             this.emptySpaceItem2.AllowHotTrack = false;
             this.emptySpaceItem2.Location = new System.Drawing.Point(0, 26);
             this.emptySpaceItem2.Name = "emptySpaceItem2";
-            this.emptySpaceItem2.Size = new System.Drawing.Size(640, 12);
+            this.emptySpaceItem2.Size = new System.Drawing.Size(640, 11);
             this.emptySpaceItem2.TextSize = new System.Drawing.Size(0, 0);
             // 
             // layoutControlItem4
@@ -186,6 +223,14 @@
             this.layoutControlItem4.Size = new System.Drawing.Size(321, 26);
             this.layoutControlItem4.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem4.TextVisible = false;
+            // 
+            // emptySpaceItem3
+            // 
+            this.emptySpaceItem3.AllowHotTrack = false;
+            this.emptySpaceItem3.Location = new System.Drawing.Point(350, 0);
+            this.emptySpaceItem3.Name = "emptySpaceItem3";
+            this.emptySpaceItem3.Size = new System.Drawing.Size(301, 24);
+            this.emptySpaceItem3.TextSize = new System.Drawing.Size(0, 0);
             // 
             // FormUniversity
             // 
@@ -199,18 +244,22 @@
             this.Load += new System.EventHandler(this.FormUniversity_Load);
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
             this.layoutControl1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.TextEditUniversityYearsInService.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SpinEditYearsInService.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.TextEditUniversityName.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl2)).EndInit();
             this.layoutControl2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BSUniversity)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -218,12 +267,10 @@
         #endregion
 
         private DevExpress.XtraLayout.LayoutControl layoutControl1;
-        private DevExpress.XtraEditors.TextEdit TextEditUniversityYearsInService;
         private DevExpress.XtraEditors.TextEdit TextEditUniversityName;
         private DevExpress.XtraLayout.LayoutControlGroup Root;
         private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
         private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem1;
-        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
         private DevExpress.XtraLayout.LayoutControl layoutControl2;
         private DevExpress.XtraEditors.SimpleButton ButtonCancel;
         private DevExpress.XtraEditors.SimpleButton ButtonSave;
@@ -231,5 +278,11 @@
         private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
         private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem2;
         private DevExpress.XtraLayout.LayoutControlItem layoutControlItem4;
+        private DevExpress.XtraEditors.SpinEdit SpinEditYearsInService;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem5;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem5;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem4;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem3;
+        private BindingSource BSUniversity;
     }
 }
--- Session-07/Session-07/FormUniversity.cs
+++ Session-07/Session-07/FormUniversity.cs
@@ -1,40 +1,28 @@
 ﻿using DevExpress.XtraEditors;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using Uni;
 
 namespace Session_07 {
     public partial class FormUniversity : XtraForm {
         private string _formName = "University";
-        public Uni.University University{ get; set; }
+        public University University { get; set; }
         public FormUniversity() {
             InitializeComponent();
         }
-
         private void FormUniversity_Load(object sender, EventArgs e) {
             this.Text = _formName;
-            TextEditUniversityName.Text = University.Name;
-            TextEditUniversityYearsInService.Text = University.YearsInService.ToString();
-        }
 
+            BSUniversity.DataSource = this.University;
+
+            TextEditUniversityName.DataBindings.Add("EditValue", BSUniversity, "Name", true);
+            SpinEditYearsInService.DataBindings.Add("EditValue", BSUniversity, "YearsInService", true);
+        }
+        #region Buttons
         private void ButtonCancel_Click(object sender, EventArgs e) {
             this.Close();
         }
-
         private void ButtonSave_Click(object sender, EventArgs e) {
-            UpdateUniversity();
-            MessageBox.Show("Changes Saved.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-        }
 
-        private void UpdateUniversity() {
-            University.Name = TextEditUniversityName.Text;
-            University.YearsInService = Convert.ToInt32(TextEditUniversityYearsInService.Text);
         }
+        #endregion
     }
 }
