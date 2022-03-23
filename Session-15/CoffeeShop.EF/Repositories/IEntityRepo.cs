a4ef1e33cb4932c6919577017639837934400abf
Jimbos <tr9l9l9@hotmail.com>
2022-03-23 16:07:24 +0200

    Added IEntityRepo

---
+++ Session-15/CoffeeShop.EF/Repositories/IEntityRepo.cs
@@ -0,0 +1,17 @@
+﻿using CoffeeShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.EF.Repositories {
+    public interface IEntityRepo<TEntity>
+        where TEntity : BaseEntity {
+        List<TEntity> GetAll();
+        TEntity? GetById(int id);
+        Task Create(TEntity entity);
+        Task Update(int id, TEntity entity);
+        Task Delete(int id);
+    }
+}
