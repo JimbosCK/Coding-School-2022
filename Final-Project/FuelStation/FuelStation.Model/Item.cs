7d503cc7842c86dbc7f256570f20983137e49a3c
Dimitris Chatziathanasiou-Karakasis <dimchatzkar@gmail.com>
2022-04-09 17:28:48 +0300

    Added entity Item

---
+++ Final-Project/FuelStation/FuelStation.Model/Item.cs
@@ -0,0 +1,12 @@
+﻿
+namespace FuelStation.Model
+{
+    public class Item
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public ItemTypeEnum ItemType { get; set; }
+        public decimal Price { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
