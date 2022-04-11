d42ec1660d550a6999599659a9c6cbdd3deb259a
Dimitris Chatziathanasiou-Karakasis <dimchatzkar@gmail.com>
2022-04-11 19:17:27 +0300

    Created LedgerViewModel. Added MonthsEnum in Enumerations.

---
+++ Final-Project/FuelStation/FuelStation.Blazor/Shared/ViewModels/LedgerViewModels.cs
@@ -0,0 +1,13 @@
+﻿
+namespace FuelStation.Blazor.Shared.ViewModels
+{
+    public class LedgerViewModel
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public decimal Income { get; set; }
+        public decimal Expenses { get; set; }
+        public decimal Total { get; set; }
+
+    }
+}
