8c420ef1b9b673cd9ea6dc118f3bd6e5aecef6f3
Dimitris Chatziathanasiou-Karakasis <dimchatzkar@gmail.com>
2022-04-13 09:27:33 +0300

    Removed old commented-out code from program.cs

--- Final-Project/FuelStation/FuelStation.Win/Program.cs
+++ Final-Project/FuelStation/FuelStation.Win/Program.cs
@@ -3,30 +3,6 @@ using Microsoft.Extensions.DependencyInjection;
 using FuelStation.Model;
 using FuelStation.Blazor.Shared.Services;
 
-/*
- public static class Program
-{
-  //..
-  public static IServiceProvider ServiceProvider { get; set; }
-
-  static void ConfigureServices()
-  {
-    var services = new ServiceCollection();
-    services.AddTransient<ISomeThing, SomeThing>();
-    ServiceProvider = services.BuildServiceProvider();
-  }
-
-  [STAThread]
-  static void Main()
-  {  
-    Application.EnableVisualStyles();
-    Application.SetCompatibleTextRenderingDefault(false);
-    ConfigureServices();
-    Application.Run(new MainForm());
-  }
-}
- * */
-
 namespace FuelStation.Win
 {
     internal static class Program
