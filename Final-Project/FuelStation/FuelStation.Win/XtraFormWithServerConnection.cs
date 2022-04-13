259f9a89c18eb9c1c20e128b8957ea64182ab639
Dimitris Chatziathanasiou-Karakasis <dimchatzkar@gmail.com>
2022-04-13 16:32:48 +0300

    [Refactor]: Removed empty lines and unused imports.

--- Final-Project/FuelStation/FuelStation.Win/XtraFormWithServerConnection.cs
+++ Final-Project/FuelStation/FuelStation.Win/XtraFormWithServerConnection.cs
@@ -1,21 +1,13 @@
 ﻿using DevExpress.XtraEditors;
 using FuelStation.Blazor.Shared.ViewModels;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
-using System.Net.Http.Json;
-
 
 namespace FuelStation.Win
 {
     public class XtraFormWithServerConnection : XtraForm
     {
-
         protected HttpClient httpClient;
 
         public XtraFormWithServerConnection()
