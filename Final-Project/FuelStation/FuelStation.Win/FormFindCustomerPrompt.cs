f6c940f6d1164edf7751f4d76625d66aee1b105c
Jimbos <tr9l9l9@hotmail.com>
2022-05-15 17:10:28 +0300

    Removed unused variable and reformated code.

--- Final-Project/FuelStation/FuelStation.Win/FormFindCustomerPrompt.cs
+++ Final-Project/FuelStation/FuelStation.Win/FormFindCustomerPrompt.cs
@@ -2,63 +2,49 @@
 using FuelStation.Blazor.Shared.ViewModels;
 using System.Net.Http.Json;
 
-namespace FuelStation.Win
-{
-    public partial class FormFindCustomerPrompt : XtraFormWithServerConnection
-    {
+namespace FuelStation.Win {
+    public partial class FormFindCustomerPrompt : XtraFormWithServerConnection {
         private bool _errorFindingUser = false;
-        private Guid _customerID;
         public string CustomerCardNumber { get; set; } = string.Empty;
-        public FormFindCustomerPrompt()
-        {
+        public FormFindCustomerPrompt() {
             InitializeComponent();
         }
-        private void FormFindCustomerPrompt_VisibleChanged(object sender, EventArgs e)
-        {
-            if (! string.IsNullOrEmpty(CustomerCardNumber))
-            {
+        private void FormFindCustomerPrompt_VisibleChanged(object sender, EventArgs e) {
+            if (!string.IsNullOrEmpty(CustomerCardNumber)) {
                 OpenTransactionLineEdit(CustomerCardNumber);
             }
         }
 
         #region Btn Clicks
-        private void btnSubmit_Click(object sender, EventArgs e)
-        {
-            try
-            {
+        private void btnSubmit_Click(object sender, EventArgs e) {
+            try {
                 labelErrors.Focus();
                 ResetErrorLabel();
                 OpenTransactionLineEdit(textEditCustomerCardNumber.Text);
 
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex) {
                 _errorFindingUser = true;
             }
             if (_errorFindingUser) ShowErrorLabel();
         }
-        private void btnNew_Click(object sender, EventArgs e)
-        {
+        private void btnNew_Click(object sender, EventArgs e) {
             var formCustomerEdit = new FormCustomerEdit(null);
             formCustomerEdit.RefFindCustomerPrompt = this;
             formCustomerEdit.ShowDialog();
             this.Hide();
         }
-        private void btnCancel_Click(object sender, EventArgs e)
-        {
+        private void btnCancel_Click(object sender, EventArgs e) {
             this.Close();
         }
 
         #endregion
 
         #region Methods
-        private async void OpenTransactionLineEdit(string customerCardNumber)
-        {
-            try
-            {
+        private async void OpenTransactionLineEdit(string customerCardNumber) {
+            try {
                 var customer = await httpClient.GetFromJsonAsync<CustomerViewModel>($"customer/cardnumber/{customerCardNumber}");
-                if(customer is null)
-                {
+                if (customer is null) {
                     _errorFindingUser = true;
                     return;
                 }
@@ -67,19 +53,16 @@ namespace FuelStation.Win
                 formTransactionEdit.ShowDialog();
                 this.Hide();
             }
-            catch(Exception e)
-            {
+            catch (Exception e) {
                 _errorFindingUser = true;
             }
             if (_errorFindingUser) ShowErrorLabel();
         }
-        private void ResetErrorLabel()
-        {
+        private void ResetErrorLabel() {
             labelErrors.Text = string.Empty;
             _errorFindingUser = false;
         }
-        private void ShowErrorLabel()
-        {
+        private void ShowErrorLabel() {
             labelErrors.Text = "Could not find Customer's Card Number.";
         }
         #endregion
